4a059b6e32747aa8c44eede0d1d78821d66382e0
PeiYun <ollylee8520@gmail.com>
2022-06-09 16:27:26 -0600

    added async/await to db.query to fixed get choices data and match role.title -> department_id, also use env in db

--- index.js
+++ index.js
@@ -1,33 +1,88 @@
+require('dotenv').config();
 const inquirer = require("inquirer");
 const questions = require("./lib/Questions");
 const mysql = require('mysql2');
+const util = require('util');
 const cTable = require('console.table');
+// const {whatToDoQ, addNewDepQ ,addNewRoleQ, addNewEmpQ,updateEmpRoleQ} = require('./lib/Question2');
 
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: 'ollyhite',
-        database: 'employee_db'
-        // user: process.env.DB_USER,
-        // password: process.env.DB_PASSWORD,
-        // database: process.env.DB_NAME
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     },
     console.log(`Connected to the employee_db database.`)
 );
-let titleArray;
-const getAllRoleTitle = () =>{
-    db.query('SELECT title FROM role GROUP BY title',function(err,result){
-        // console.log(err);
-        // console.log(result);
-        titleArray = result.map((item)=>item.title)
-        console.log("titleArray",titleArray);
-        console.log(typeof titleArray);
+const query = util.promisify(db.query).bind(db);
+
+const getAllRoleTitle = async() =>{
+    try{
+        const result = await query('SELECT title FROM role GROUP BY title');
+        const titleArray = result.map((item)=>item.title);
         return titleArray;
-    });
+    }catch(err){
+    
+    }
 }
 
-const roleQuestion =[{
+
+
+const start = async() =>{
+    const answer = await inquirer.prompt(questions.whatToDo);
+            switch(answer.userTodo){
+                case "View All Employee": await viewAllEmp();
+                break;
+                case "Add Employee": await addNewEmp();
+                break;
+                case "Update Employee Role": await updateEmpRole();
+                break;
+                case "View All Role": await viewAllRole();
+                break;
+                case "Add Role": await addNewRole();
+                break;
+                case "View All Departments": await viewAllDep();
+                break;
+                case "Add Departments": await addNewDep();
+                break;
+                case "Quit": quit();
+                break;
+            }
+}
+
+
+
+const viewAllEmp = async() =>{
+    try{
+        const viewAllEmp = await query('SELECT employee.id,employee.first_name,employee.last_name,role.title,department.department_name,role.salary,employee.manager_id FROM department JOIN role ON department.id = role.department_id join employee on role.id = employee.role_id;')
+        console.table(viewAllEmp);
+        start();
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const addNewEmp = async() =>{
+    
+}
+
+const updateEmpRole = async() =>{
+    
+}
+
+const viewAllRole = async() =>{
+    try{
+        const viewAllRole = await query('SELECT * FROM role')
+        console.table(viewAllRole);
+        start();
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const addNewRole = async () =>{
+    const roleQuestion =[{
         type:"input",
         message:"What is the name of the role?",
         name:"roleName"
@@ -39,99 +94,56 @@ const roleQuestion =[{
     },{
         type:"list",
         message:"Which department dose the role belong to?",
-        choices: titleArray,
+        choices: await getAllRoleTitle(),
         name:"departmentChoose"
     }]
-
-
-
-const start = () =>{
-    inquirer.prompt(questions.whatToDo).then((answer) =>{
-            console.log("user want to do",answer);
-            switch(answer.userTodo){
-                case "View All Employee": viewAllEmp();
-                break;
-                case "Add Employee": addNewEmp();
-                break;
-                case "Update Employee Role": updateEmpRole();
-                break;
-                case "View All Role": viewAllRole();
-                break;
-                case "Add Role": addNewRole();
-                break;
-                case "View All Departments": viewAllDep();
-                break;
-                case "Add Departments": addNewDep();
-                break;
-                case "Quit": quit();
-                break;
-            }
-        })
-}
-
-
-
-const viewAllEmp = () =>{
-    db.query('SELECT employee.id,employee.first_name,employee.last_name,role.title,department.department_name,role.salary,employee.manager_id FROM department JOIN role ON department.id = role.department_id join employee on role.id = employee.role_id;',function(err,result){
-        // console.log(result);
-        console.table(result);
-        start();
-    });
-}
-
-const addNewEmp = () =>{
-    
-}
-
-const updateEmpRole = () =>{
-    
-}
-
-const viewAllRole = () =>{
-    db.query('SELECT * FROM role',function(err,result){
-        // console.log(result);
-        console.table(result);
-        start();
-    });
-}
-
-const addNewRole = () =>{
-    inquirer.prompt(roleQuestion).then((answer) =>{
+    const answer = await inquirer.prompt(roleQuestion);
         const roleAnswerObj = {
-            roleName: answer.roleName,
-            roleSalary: answer.roleSalary,
-            departmentChoose: answer.departmentChoose,
+            title: answer.roleName,
+            salary: answer.roleSalary,
+            department_name: answer.departmentChoose,
         }
-        console.log("roleAnswerObj",roleAnswerObj);
-        // db.query(`INSERT INTO department(department_name)
-        // VALUES('${answer.roleName}')`,function(err,result){
-        //     err ? console.log(err):
-        //         // console.log(result);
-        //         console.log(`Added ${answer.roleName} to the database`);
-        //         start();
-        //         });
-    });
+        addRoleToDB(roleAnswerObj)
 }
 
-const viewAllDep = () =>{
-    db.query('SELECT * FROM department',function(err,result){
-        // console.log(result);
-        console.table(result);
+const addRoleToDB = async(roleAnswerObj)=>{
+    try{
+        const roleTitleAndId = await query('SELECT id,title FROM role GROUP BY id');
+        const findId =roleTitleAndId.find((item => item.title === roleAnswerObj.department_name))
+        const setToDB = await query(`INSERT INTO role(title,salary,department_id)
+        VALUES ('${roleAnswerObj.title}', ${Number(roleAnswerObj.salary)},${findId.id});`);
+        console.log(`Added ${roleAnswerObj.title} to the database`);
+        const viewAllRole = await query('SELECT * FROM role')
+        console.table(viewAllRole);
         start();
-    });
+    }catch(err){
+        console.log(err);
+    }
 }
 
-const addNewDep = () =>{
-    inquirer.prompt(questions.addNewDep).then((answer) =>{
-        console.log(answer.departName);
-        db.query(`INSERT INTO department(department_name)
-        VALUES('${answer.departName}')`,function(err,result){
-            err ? console.log(err):
-                // console.log(result);
-                console.log(`Added ${answer.departName} to the database`);
-                start();
-                });
-    });
+const viewAllDep = async() =>{
+    try{
+        const viewAllDep = await query('SELECT * FROM department')
+        console.table(viewAllDep);
+        start();
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const addNewDep = async() =>{
+    const answer = await inquirer.prompt(questions.addNewDep);
+        // console.log(answer.departName);
+    try{
+        const setToDB = await query(`INSERT INTO department(department_name)
+        VALUES('${answer.departName}')`)
+        console.log(`Added ${answer.departName} to the database`);
+        const viewAllDep = await query('SELECT * FROM department')
+        console.table(viewAllDep);
+        start();
+    }catch(err){
+        console.log(err)
+    }
 }
 
 
@@ -141,5 +153,4 @@ const quit = () =>{
 }
 
 
-getAllRoleTitle()
 start();
